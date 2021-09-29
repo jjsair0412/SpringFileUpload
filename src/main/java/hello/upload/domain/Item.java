4c5747f3000cd079be17821422c81e726dd29c89
주진성 <jjsair0412@naver.com>
2021-09-29 18:39:59 +0900

    주석 추가

--- src/main/java/hello/upload/domain/Item.java
+++ src/main/java/hello/upload/domain/Item.java
@@ -9,5 +9,5 @@ public class Item {
     private Long id;
     private String itemName;
     private UploadFile attachFile;
-    private List<UploadFile> imageFiles;
+    private List<UploadFile> imageFiles; // 여러개의 파일들이 들어오면 , List로 받아주면 다들어올수 있다.
 }
